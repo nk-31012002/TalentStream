1ea7570efbabb2bd31d83b14312726fa8dccf92a
Nagendra Kumar <102040326+nk-31012002@users.noreply.github.com>
2026-07-02 19:21:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Job App',
+      title: 'TalentStream',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
